02ef2f1cb4e1e0bf3696ea68b73d0d9c3b9c8657
Pierre-Luc Beaudoin <pierre-luc.beaudoin@novopia.com>
2009-11-25 11:44:06 -0500

    Add links to previous and next month

--- events/views.py
+++ events/views.py
@@ -1,7 +1,13 @@
-from datetime import date
+from datetime import date, timedelta
 from django.shortcuts import render_to_response
 
 def month (request, year, month):
   month = date(int(year), int(month), 1)
+  previous = month - timedelta(days=15)
+  next = month + timedelta(days=45)
 
-  return render_to_response('events/event_archive_month.html', {'month': month})
+  return render_to_response('events/event_archive_month.html', {
+    'month': month,
+    'previous_month': previous,
+    'next_month': next,
+    })
