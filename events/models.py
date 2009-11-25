3eaa7898de9f9769f432003a0c03a5a207abea85
Pierre-Luc Beaudoin <pierre-luc.beaudoin@novopia.com>
2009-11-24 23:49:25 -0500

    Initial data for Cities and Regions

--- events/models.py
+++ events/models.py
@@ -5,9 +5,19 @@ class Region (models.Model):
   id = models.PositiveSmallIntegerField(primary_key=True)
   name = models.CharField (max_length=200)
 
+  def __unicode__ (self):
+    return self.name
+
 class City (models.Model):
+  class Meta:
+    verbose_name_plural = "cities"
   name = models.CharField (max_length=200)
   region = models.ForeignKey(Region)
+  latitude = models.FloatField ()
+  longitude = models.FloatField ()
+
+  def __unicode__ (self):
+    return self.name + " (" + self.region.name + ")"
 
 class Event (models.Model):
   title = models.CharField (max_length=200)
@@ -32,4 +42,7 @@ class Event (models.Model):
   moderated = models.BooleanField (default=False)
   submitter = models.ForeignKey(User)
   decision_time = models.DateTimeField (blank=True);
+
+  def __unicode__ (self):
+    return self.title
 
