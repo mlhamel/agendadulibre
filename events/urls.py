2b506f71486137f40e2da9b2773a0240eceab408
Pierre-Luc Beaudoin <pierre-luc.beaudoin@novopia.com>
2009-11-25 11:22:03 -0500

    Remove generic month view
    Since the calendar queries himself the Events, no need to use
    this generic view

--- events/urls.py
+++ events/urls.py
@@ -12,22 +12,12 @@ list_info = {
     "paginate_by": 25,
 }
 
-month_list_info = {
-    "month_format": "%m",
-    "date_field": "start_time",
-    "allow_future": True,
-    "allow_empty": True,
-}
-
 event_info = general_info
 event_list_info = dict(general_info, **list_info)
-event_list_month_info = dict(general_info, **month_list_info)
-
-
 
 urlpatterns = patterns('',
     (r'^$', list_detail.object_list, event_list_info),
     (r'^(?P<object_id>\d+)/$', list_detail.object_detail, event_info),
-    (r'^(?P<year>\d+)/(?P<month>\d+)/$', date_based.archive_month, event_list_month_info),
+    (r'^(?P<year>\d+)/(?P<month>\d+)/$', 'events.views.month'),
 )
 
--- events/views.py
+++ events/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from datetime import date
+from django.shortcuts import render_to_response
+
+def month (request, year, month):
+  month = date(int(year), int(month), 1)
+
+  return render_to_response('events/event_archive_month.html', {'month': month})
