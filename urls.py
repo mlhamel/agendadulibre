fd88145981f85afb76cfbb8b52884fd0048865f9
Pierre-Luc Beaudoin <pierre-luc.beaudoin@novopia.com>
2009-11-25 19:44:57 -0500

    Add about page

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
 
 from django.contrib import admin
 admin.autodiscover()
@@ -6,6 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     (r'^event/', include('agenda.events.urls')),
     (r'^$', 'agenda.views.index'),
+    (r'^about/$', direct_to_template, {'template': 'about.html'}),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
