38833f68daabe845650250e3edf9cb4b3cc9cb62
Pierre-Luc Beaudoin <pierre-luc.beaudoin@novopia.com>
2009-11-26 23:27:19 -0500

    hack: Print date in fr_CA locale

--- events/templatetags/humantime.py
+++ events/templatetags/humantime.py
@@ -3,6 +3,7 @@
 from django.template.defaultfilters import stringfilter
 from datetime import datetime, timedelta
 from django import template
+import locale
 
 register = template.Library()
 
@@ -11,10 +12,14 @@ def event_time(start, end):
     today = datetime.today ()
     result = ""
 
+    # Hack! get the correct user local from the request
+    loc = locale.getlocale()
+    locale.setlocale(locale.LC_ALL, 'fr_CA.UTF8')
+
     if start == today:
-        result += "aujourd'hui "
+        result += "Aujourd'hui "
     else:
-        result += "le %s " % start.strftime ("%A %d %B %Y")
+        result += "Le %s " % start.strftime ("%A %d %B %Y")
 
     if start.day == end.day and start.month == end.month and start.year == end.year:
         result += "de %s " % start.strftime ("%H:%M")
@@ -23,5 +28,6 @@ def event_time(start, end):
         result += "à %s" % start.strftime ("%H:%M")
         result += "jusqu'au %s" % end.strftime ("%A %d %B %Y à %H:%M")
 
+    locale.setlocale(locale.LC_ALL, loc)
     return result
 
