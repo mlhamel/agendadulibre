32a88dd99098d074e71cf354feb51ac30272250a
Pierre-Luc Beaudoin <pierre-luc.beaudoin@novopia.com>
2009-11-28 16:47:16 -0500

    Add a first try at the moderation admin forms

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,4 @@
+# -*- encoding:utf-8 -*-
 #
 # Copyright (C) 2009 Novopia Solutions Inc.
 #
@@ -19,10 +20,33 @@
 
 from django.contrib import admin
 from agenda.events.models import Event, City, Region
+from datetime import datetime
+from django.core.mail import mail_admins
+
+def make_published(self, request, queryset):
+    rows_updated = queryset.update(moderated=True,moderator=request.user,decision_time=datetime.now())
+    if rows_updated == 1:
+        message_bit = u"1 événement a été "
+    else:
+        message_bit = u"%s événements ont été " % rows_updated
+    self.message_user(request, u"%s publié." % message_bit)
+
+make_published.short_description = u"Publier les événements choisis"
+
+def make_unpublished(self, request, queryset):
+    rows_updated = queryset.update(moderated=False,moderator=request.user,decision_time=datetime.now())
+    if rows_updated == 1:
+        message_bit = u"1 événement a été "
+    else:
+        message_bit = u"%s événements ont été " % rows_updated
+    self.message_user(request, u"%s non publié." % message_bit)
+
+make_unpublished.short_description = u"Non publier les événements choisis"
 
 class EventAdmin(admin.ModelAdmin):
     date_hierarchy = 'start_time'
     list_filter = ('moderated',)
+    actions = [make_published,make_unpublished]
 
 class CityAdmin(admin.ModelAdmin):
     list_filter = ('region',)
@@ -32,3 +56,4 @@ class CityAdmin(admin.ModelAdmin):
 admin.site.register(Event, EventAdmin)
 admin.site.register(Region)
 admin.site.register(City, CityAdmin)
+
