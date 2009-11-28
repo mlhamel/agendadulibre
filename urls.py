383f552bb6618fbf00e957c2b92ec0ab975cfdc6
Pierre-Luc Beaudoin <pierre-luc.beaudoin@novopia.com>
2009-11-28 00:22:22 -0500

    Add licence information in each files

--- urls.py
+++ urls.py
@@ -1,3 +1,22 @@
+#
+# Copyright (C) 2009 Novopia Solutions Inc.
+#
+# Author: Pierre-Luc Beaudoin <pierre-luc.beaudoin@novopia.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 
@@ -9,10 +28,8 @@ urlpatterns = patterns('',
     (r'^$', 'agenda.views.index'),
     (r'^about/$', direct_to_template, {'template': 'about.html'}),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/', include(admin.site.urls)),
 
-    # Uncomment the next line to enable the admin:
-     (r'^admin/', include(admin.site.urls)),
+#    (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+#        {'document_root': 'PATH.../templates/media'}),
 )
