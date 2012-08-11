02695cce22798b17d4f6c9c26f58fc785d249506
mlhamel <none@none>
2012-08-11 19:04:21 +0000

    fix prod conf

--- agenda/production.py
+++ agenda/production.py
@@ -1,11 +1,15 @@
 from agenda.settings import *
 
-DATABASE_ENGINE = 'mysql'
-DATABASE_NAME = 'agenda'
-DATABASE_USER = 'agenda'
-DATABASE_PASSWORD = 'Eegh0Im7'
-DATABASE_HOST = '127.0.0.1'
-DATABASE_PORT = '3306'
+DATABASES = {
+    "default": {
+        "ENGINE": 'django.db.backends.mysql',
+        "NAME": 'agenda',
+        "USER": 'agenda',
+        "PASSWORD": 'Eegh0Im7',
+        "HOST": '127.0.0.1',
+        "PORT": '3306',
+        }
+    }
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
