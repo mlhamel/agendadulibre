17dc61bd40c529fc9e737649ff570a95453b798b
mlhamel <none@none>
2012-08-15 13:35:16 +0000

    Ajout d'un middleware manquant

--- agenda/settings.py
+++ agenda/settings.py
@@ -67,6 +67,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
 )
 
 ROOT_URLCONF = 'agenda.urls'
