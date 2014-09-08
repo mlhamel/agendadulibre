67a47826828163b8ba3b77030fe6f16e105dedc7
Mathieu Leduc-Hamel <mlhamel@ajah.ca>
2014-09-08 17:58:47 -0400

    [#19] fixe l'indentation

--- agenda/events/views.py
+++ agenda/events/views.py
@@ -30,87 +30,95 @@ from django.core.mail import mail_admins
 from django.conf import settings
 
 
-def propose (request):
-  form = EventForm (request)
+def propose(request):
+    form = EventForm(request)
 
-  if request.method == 'POST':
-    form = EventForm(request.POST)
-    if form.is_valid():
-      e = form.save()
-      if settings.ENABLE_MAIL:
-        msg = u"Bonjour, \n\nLe nouvel évènement '" + e.title + u"' a été soumis.  Pour le réviser, veuillez visiter\n"
-        msg += u"http://www.agendadulibre.qc.ca/admin/events/event/%d/" % e.id
-        msg += u"\n\nMerci,\n\nL'Agenda du libre du Québec"
-        mail_moderators (u"Nouvel évènement en attente de modération", msg)
-      return HttpResponseRedirect('/event/new/thanks/')
-  else:
-    form = EventForm()
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            e = form.save()
+            if settings.ENABLE_MAIL:
+                msg = (u"Bonjour, \n\nLe nouvel évènement '" + e.title
+                       + u"' a été soumis.  Pour le réviser, veuillez visiter\n")
+                msg += u"http://www.agendadulibre.qc.ca/admin/events/event/%d/" % e.id
+                msg += u"\n\nMerci,\n\nL'Agenda du libre du Québec"
+                mail_moderators(u"Nouvel évènement en attente de modération",
+                                msg)
+            return HttpResponseRedirect('/event/new/thanks/')
+    else:
+        form = EventForm()
 
-  return render_to_response('events/event_new.html', {
-    'form': form,
+    return render_to_response('events/event_new.html', {
+        'form': form,
     })
+
 
 def region_cmp (a, b):
-  return a['value'] - b['value']
+    return a['value'] - b['value']
 
 
-def stats (request):
-  total = Event.objects.filter(moderated=True).aggregate(Count('id'))['id__count']
-  total_to_moderate = Event.objects.filter(moderated=False).aggregate(Count('id'))['id__count']
+def stats(request):
+    total = (Event.objects
+             .filter(moderated=True)
+             .aggregate(Count('id'))['id__count'])
+    total_to_moderate = (Event.objects
+                         .filter(moderated=False)
+                         .aggregate(Count('id'))['id__count'])
 
-  region_list = []
-  regions = Region.objects.all()
-  for region in regions:
-     region_list.append ({
-       'name': region.name,
-       'value': Event.objects.filter(moderated=True,city__region=region).aggregate(Count('id'))['id__count']
-       })
-  region_list.sort (region_cmp, reverse=True)
+    region_list = []
+    regions = Region.objects.all()
+    for region in regions:
+        region_list.append({
+            'name': region.name,
+            'value': (Event.objects
+                      .filter(moderated=True,city__region=region)
+                      .aggregate(Count('id'))['id__count'])
+        })
+    region_list.sort(region_cmp, reverse=True)
 
-  return render_to_response('events/stats.html', {
-    'region_list': region_list,
-    'total': total,
-    'total_to_moderate': total_to_moderate,
+    return render_to_response('events/stats.html', {
+        'region_list': region_list,
+        'total': total,
+        'total_to_moderate': total_to_moderate,
     })
 
 
-def feed_list (request):
+def feed_list(request):
+    region_list = Region.objects.all()
 
-  region_list = Region.objects.all()
-
-  return render_to_response('events/feeds.html', {
-    'region_list': region_list,
+    return render_to_response('events/feeds.html', {
+        'region_list': region_list,
     })
 
-def calendar_region (request, region_id):
-  try:
-    region = Region.objects.get(pk=region_id)
-  except Region.DoesNotExist:
-    return HttpResponseNotFound ()
-  callable = UpcomingEventCalendarByRegion (region)
 
-  return callable (request)
+def calendar_region(request, region_id):
+    try:
+        region = Region.objects.get(pk=region_id)
+    except Region.DoesNotExist:
+        return HttpResponseNotFound()
+    callable = UpcomingEventCalendarByRegion(region)
+    return callable(request)
 
 
-def month (request, year, month):
-  month = date(int(year), int(month), 1)
-  previous = month - timedelta(days=15)
-  next = month + timedelta(days=45)
+def month(request, year, month):
+    month = date(int(year), int(month), 1)
+    previous = month - timedelta(days=15)
+    next = month + timedelta(days=45)
 
-  form = RegionFilterForm(request)
+    form = RegionFilterForm(request)
 
-  region = None
-  if request.method == 'GET':
-    form = RegionFilterForm(request.GET)
-    if form.is_valid():
-      region = form.cleaned_data['region']
-  else:
-    form = RegionFilterForm()
+    region = None
+    if request.method == 'GET':
+        form = RegionFilterForm(request.GET)
+        if form.is_valid():
+            region = form.cleaned_data['region']
+    else:
+        form = RegionFilterForm()
 
-  return render_to_response('events/event_archive_month.html', {
-    'month': month,
-    'previous_month': previous,
-    'next_month': next,
-    'form': form,
-    'region': region,
+    return render_to_response('events/event_archive_month.html', {
+        'month': month,
+        'previous_month': previous,
+        'next_month': next,
+        'form': form,
+        'region': region,
     })
